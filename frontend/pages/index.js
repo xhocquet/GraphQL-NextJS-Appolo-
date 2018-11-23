80e3773046758d49917cc3eec1233de97f132c17
Xavier Hocquet <xhocquet@gmail.com>
2018-11-23 11:44:29 -0700

    Initial commit through module #2

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+const Home = props => {
+  return (
+    <div>
+      <h1>Hullo</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+const Sell = props => {
+  return (
+    <div>
+      <h1>Sell</h1>
+    </div>
+  );
+};
+
+export default Sell;
